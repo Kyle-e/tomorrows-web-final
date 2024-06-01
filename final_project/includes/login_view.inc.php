76ff4d4a65834fe10668cf4881a4ced9b2a74f52
Kyle-e <91416577+Kyle-e@users.noreply.github.com>
2024-06-02 02:53:11 +0400

    Update login_view.inc.php
    Added function: display username when logged in,

--- final_project/includes/login_view.inc.php
+++ final_project/includes/login_view.inc.php
@@ -3,6 +3,15 @@
 
 declare(strict_types=1);
 
+function show_username() {
+    if (isset($_SESSION["user_id"])) {
+        echo "Logged in as " . $_SESSION["user_username"];        
+    } else {
+        echo "Guest";
+    }
+
+}
+
 function check_login_errors() {
     if (isset($_SESSION["errors_login"])) {
         $errors = $_SESSION["errors_login"];
