0c4bca7dad3c2dbd49f448548cdbe8c6abdeabb5
Kyle-e <91416577+Kyle-e@users.noreply.github.com>
2024-06-01 19:43:55 +0400

    Create signup_cont.inc.php

---
+++ final_project/includes/signup_cont.inc.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types= 1);
+
+function is_input_empty(string $username, string $pswd, string $email) {
+    if(empty($username) || empty($pswd) || empty($email)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function is_email_invalid(string $email) {
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function is_user_taken(object $conn, string $username) {
+    if (get_username($conn, $username)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function is_email_registered(object $conn, string $email) {
+    if (get_email($conn, $email)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function create_user(object $conn, string $pswd, string $username, string $email) {
+    set_user($conn, $pswd, $username, $email);
+}
