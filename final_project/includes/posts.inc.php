3f6a8c6632c64f1c1384b43f314b34f59efffbd1
Kyle-e <91416577+Kyle-e@users.noreply.github.com>
2024-06-04 21:54:16 +0400

    Create posts.inc.php

---
+++ final_project/includes/posts.inc.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'dbc.inc.php';
+
+if(!isset($_SESSION["user_id"])) {
+    header("Location: ../index.php");
+    die();
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $userId = $_POST["user_id"];
+    $post = $_POST["post"];
+
+    require_once 'config_session.inc.php';
+
+
+    try {
+        require_once 'dbc.inc.php';
+        require_once 'posts_model.inc.php';
+        require_once 'posts_cont.inc.php';
+
+        create_post($conn, $userId, $post);
+
+        header("Location: ../main.php?post=created");
+
+        $conn = null;
+        $stmt = null;        
+
+        die();
+
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+} else {
+    echo "Request error";
+}
