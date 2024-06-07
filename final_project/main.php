0e4631cc61ee4fe827ff38eb60e65fa9f81cd8ee
Kyle-e <91416577+Kyle-e@users.noreply.github.com>
2024-06-07 22:03:48 +0400

    Update main.php

--- final_project/main.php
+++ final_project/main.php
@@ -2,7 +2,6 @@
 require_once 'includes/dbc.inc.php';
 require_once 'includes/config_session.inc.php';
 require_once 'includes/login_view.inc.php';
-require_once 'includes/posts_view.inc.php';
 require_once 'includes/posts_model.inc.php';
 
 $posts = display_posts($conn);
@@ -18,57 +17,71 @@ $quote = json_decode(file_get_contents($apiLink));
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="stylesheets/main.css">
+    <title>Home</title>
 </head>
 <body>
 
-    <h4>
+    <!-- DISPLAY IF USER IS LOGGED IN OR NOT -->
+    <div id="login-box">
+        <h4 id="login-state">
+            <?php
+            show_username();
+            ?>
+        </h4>
+
         <?php
-        show_username();
-        ?>
-    </h4>
+            if (isset($_SESSION["user_id"])) { ?>
+                <form action="includes/logout.inc.php" method="post">
+                    <br>
+                    <button>Logout</button>
+                </form>      
+        <?php } ?>
+    </div>
 
-    <?php
+    <div id="create-post">
+        <?php
+
+        // CREATE POST
         if (isset($_SESSION["user_id"])) { ?>
-            <form action="includes/logout.inc.php" method="post">
+            <form action="includes/posts.inc.php" method="post">
+                <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id']; ?>">
+                <textarea name="post" placeholder="Write a post"></textarea>
                 <br>
-                <button>Logout</button>
-            </form>      
-    <?php } ?>
+                <button>Post</button>
+            </form>
+        <?php } ?>
+    </div>     
+    
+    <div id="posts">
 
-    <?php
 
-    // CREATE POST
-    if (isset($_SESSION["user_id"])) { ?>
-        <form action="includes/posts.inc.php" method="post">
-        <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id']; ?>">
-        <input name="post" placeholder="Write a post"></input>
-        <button>Post</button>
-        </form>
-    <?php } ?>
+        <?php
 
-    <?php
+        ?>
 
-    ?>
-
-    <?php foreach ($posts as $post): ?>
-    <div class="post">
-        <h2><?php echo htmlspecialchars($post["post"]); ?></h2>
-        <p>Posted by <?php echo htmlspecialchars($post["user_id"]); ?> at <?php echo htmlspecialchars($post['timestamp']); ?></p>
-    </div>
-    <?php endforeach; ?>
-
-<footer>
-    <div>
-        <h4><?php echo $quote->content;?></h4>
-        <h5><?php echo $quote->author;?></h5>
+        <?php foreach ($posts as $post): ?>
         <div>
-            <?php foreach($quote->tags as $tag) { ?>
-                <span>
-                    # <?php echo $tag;?>
-                </span>
-            <?php } ?>
+            <h3 id="post"><?php echo htmlspecialchars($post["post"]); ?></h3>
+            <p>Posted by <?php echo htmlspecialchars($post["user_id"]); ?> at <?php echo htmlspecialchars($post['timestamp']); ?></p>
+            <br>
         </div>
+        <?php endforeach; ?>
     </div>
-</footer>
+
+    <footer>
+        <div>
+            <h4><?php echo $quote->content;?></h4>
+            <h5><?php echo $quote->author;?></h5>
+            <div>
+                <?php foreach($quote->tags as $tag) { ?>
+                    <span>
+                        # <?php echo $tag;?>
+                    </span>
+                <?php } ?>
+            </div>
+        </div>
+    </footer>
+
+
 </body>
 </html>
