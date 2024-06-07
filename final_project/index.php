7926620ef4ce1648c882a01e1efaecce83eed82e
Kyle-e <91416577+Kyle-e@users.noreply.github.com>
2024-06-07 22:04:20 +0400

    Update index.php

--- final_project/index.php
+++ final_project/index.php
@@ -2,7 +2,7 @@
 require_once 'includes/config_session.inc.php';
 require_once 'includes/signup_view.inc.php';
 require_once 'includes/login_view.inc.php';
-?>
+?>  
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,19 +11,22 @@ require_once 'includes/login_view.inc.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="stylesheets/index.css">
+    <title>Login</title>
 </head>
 
 <body>
 
-    <h3>
-        <?php
-        show_username();
-        ?>
-    </h3>
+    <div id="login_box">
+        <h4 id="login-state">
+            <?php
+            show_username();
+            ?>
+        </h4>
+    </div>
 
     <?php
     if (!isset($_SESSION["user_id"])) { ?>
-        <h2>Log in</h2>
+        <h2 class="index-header">Log in</h2>
 
         <form action="includes/login.inc.php" method="post">
             <input type="text" name="username" placeholder="Username">
@@ -36,7 +39,7 @@ require_once 'includes/login_view.inc.php';
     check_login_errors();
     ?>
 
-    <h2>Sign up</h2>
+    <h2 class="index-header">Sign up</h2>
     <form action="includes/signup.inc.php" method="post">
         <?php
         signup_inputs();
